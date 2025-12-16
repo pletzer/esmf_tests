1ee9fad6d9a4e5728d9f4f0060b24d0b265b4c88
Alexander Pletzer <alexander@gokliya.net>
2025-12-16 14:34:30 +1300

    a working coupler without esmf

--- coupled/component_mod.F90
+++ coupled/component_mod.F90
@@ -1,41 +1,60 @@
 module component_mod
   use mpi
   implicit none
+  integer, parameter :: dp = kind(1.0d0)
+
+  type :: component_type
+     integer :: comp_comm
+     integer :: comp_rank, comp_size
+     real(dp), allocatable :: export_data(:)
+     real(dp), allocatable :: import_data(:)
+  end type component_type
+
 contains
 
-  subroutine comp_init(comm, name)
-    integer, intent(in) :: comm
-    character(*), intent(in) :: name
-    integer :: rank, ierr
+  subroutine comp_init(comp, comm, n, name)
+    type(component_type), intent(inout) :: comp
+    integer, intent(in)                 :: comm, n
+    character(*), intent(in)            :: name
+    integer :: ierr
 
-    call MPI_Comm_rank(comm, rank, ierr)
-    if (rank == 0) then
+    comp%comp_comm = comm
+    call MPI_Comm_rank(comm, comp%comp_rank, ierr)
+    call MPI_Comm_size(comm, comp%comp_size, ierr)
+
+    allocate(comp%export_data(n))
+    allocate(comp%import_data(n))
+
+    comp%export_data = 0.0_dp
+    comp%import_data = -1.0_dp
+
+    if (comp%comp_rank == 0) then
       write(*,*) trim(name), ": init"
     end if
   end subroutine comp_init
 
-  subroutine comp_step(comm, name, nsteps)
-    integer, intent(in) :: comm, nsteps
-    character(*), intent(in) :: name
-    integer :: rank, step, ierr
 
-    call MPI_Comm_rank(comm, rank, ierr)
+  subroutine comp_step(comp, step, name)
+    type(component_type), intent(inout) :: comp
+    integer, intent(in)                 :: step
+    character(*), intent(in)            :: name
 
-    do step = 1, nsteps
-      if (rank == 0) then
-        write(*,*) trim(name), ": step ", step
-      end if
-      call MPI_Barrier(comm, ierr)
-    end do
+    ! Dummy physics
+    comp%export_data = step + comp%comp_rank
+
+    if (comp%comp_rank == 0) then
+      write(*,*) trim(name), ": step", step
+    end if
   end subroutine comp_step
 
-  subroutine comp_finalize(comm, name)
-    integer, intent(in) :: comm
-    character(*), intent(in) :: name
-    integer :: rank, ierr
 
-    call MPI_Comm_rank(comm, rank, ierr)
-    if (rank == 0) then
+  subroutine comp_finalize(comp, name)
+    type(component_type), intent(inout) :: comp
+    character(*), intent(in)            :: name
+
+    deallocate(comp%export_data, comp%import_data)
+
+    if (comp%comp_rank == 0) then
       write(*,*) trim(name), ": finalize"
     end if
   end subroutine comp_finalize
--- coupled/coupled_driver.F90
+++ coupled/coupled_driver.F90
@@ -3,43 +3,144 @@ program coupled_driver
   use component_mod
   implicit none
 
-  integer :: ierr, world_size, world_rank
+  integer :: ierr, world_rank, world_size
+  integer :: color
   integer :: comp_comm
-  integer :: color, key
-  integer :: nsteps
+  integer :: coupler_comm
+  integer :: coupler_rank
 
+  logical :: isA, isB
+  type(component_type) :: compA, compB
+
+  integer, parameter :: n = 4
+  integer :: step
+
+  ! Coupler-owned buffers and windows
+  real(dp), allocatable :: A2B_buf(:), B2A_buf(:)
+  integer :: A2B_win, B2A_win
+
+  !----------------------------------------
+  ! Init MPI
+  !----------------------------------------
   call MPI_Init(ierr)
-  call MPI_Comm_size(MPI_COMM_WORLD, world_size, ierr)
   call MPI_Comm_rank(MPI_COMM_WORLD, world_rank, ierr)
+  call MPI_Comm_size(MPI_COMM_WORLD, world_size, ierr)
 
   !----------------------------------------
-  ! Split world into two components
-  !----------------------------------------
-  ! First half -> component A
-  ! Second half -> component B
+  ! Split WORLD into A and B
   !----------------------------------------
   if (world_rank < world_size/2) then
-    color = 0   ! component A
+    color = 0
+    isA   = .true.
+    isB   = .false.
   else
-    color = 1   ! component B
+    color = 1
+    isA   = .false.
+    isB   = .true.
   end if
-  key = world_rank
 
-  call MPI_Comm_split(MPI_COMM_WORLD, color, key, comp_comm, ierr)
+  call MPI_Comm_split(MPI_COMM_WORLD, color, world_rank, comp_comm, ierr)
 
-  nsteps = 5
+  if (isA) call comp_init(compA, comp_comm, n, "Component A")
+  if (isB) call comp_init(compB, comp_comm, n, "Component B")
 
-  select case (color)
-  case (0)
-    call comp_init(comp_comm, "Component A")
-    call comp_step(comp_comm, "Component A", nsteps)
-    call comp_finalize(comp_comm, "Component A")
+  !----------------------------------------
+  ! Coupler communicator: rank 0 of A and B
+  !----------------------------------------
+  if ((isA .and. compA%comp_rank == 0) .or. &
+      (isB .and. compB%comp_rank == 0)) then
+    call MPI_Comm_split(MPI_COMM_WORLD, 0, world_rank, coupler_comm, ierr)
+  else
+    call MPI_Comm_split(MPI_COMM_WORLD, MPI_UNDEFINED, world_rank, coupler_comm, ierr)
+  end if
 
-  case (1)
-    call comp_init(comp_comm, "Component B")
-    call comp_step(comp_comm, "Component B", nsteps)
-    call comp_finalize(comp_comm, "Component B")
-  end select
+  if (coupler_comm /= MPI_COMM_NULL) then
+    call MPI_Comm_rank(coupler_comm, coupler_rank, ierr)
+
+    allocate(A2B_buf(n), B2A_buf(n))
+    A2B_buf = -100.0_dp
+    B2A_buf = -200.0_dp
+
+    call MPI_Win_create(A2B_buf, int(n*8, kind=MPI_ADDRESS_KIND), 8, MPI_INFO_NULL, coupler_comm, A2B_win, ierr)
+    call MPI_Win_create(B2A_buf, int(n*8, kind=MPI_ADDRESS_KIND), 8, MPI_INFO_NULL, coupler_comm, B2A_win, ierr)
+  else
+    A2B_win = MPI_WIN_NULL
+    B2A_win = MPI_WIN_NULL
+  end if
+
+  !----------------------------------------
+  ! Time stepping
+  !----------------------------------------
+  do step = 1, 3
+
+    !==============================
+    ! Component A step
+    !==============================
+    if (isA) call comp_step(compA, step, "Component A")
+
+    ! A pushes to B
+    if (isA .and. compA%comp_rank == 0) then
+      call MPI_Win_lock(MPI_LOCK_SHARED, 1, 0, A2B_win, ierr)
+      call MPI_Put(compA%export_data, n, MPI_DOUBLE_PRECISION, &
+                   1, 0_MPI_ADDRESS_KIND, n, MPI_DOUBLE_PRECISION, A2B_win, ierr)
+      call MPI_Win_unlock(1, A2B_win, ierr)
+    end if
+
+    call MPI_Barrier(MPI_COMM_WORLD, ierr)
+
+    ! B pulls from A
+    if (isB .and. compB%comp_rank == 0) then
+      call MPI_Win_lock(MPI_LOCK_SHARED, 0, 0, A2B_win, ierr)
+      call MPI_Get(compB%import_data, n, MPI_DOUBLE_PRECISION, &
+                   0, 0_MPI_ADDRESS_KIND, n, MPI_DOUBLE_PRECISION, A2B_win, ierr)
+      call MPI_Win_unlock(0, A2B_win, ierr)
+    end if
+
+    if (isB) then
+      call MPI_Bcast(compB%import_data, n, MPI_DOUBLE_PRECISION, 0, comp_comm, ierr)
+    end if
+
+    !==============================
+    ! Component B step
+    !==============================
+    if (isB) call comp_step(compB, step, "Component B")
+
+    ! B pushes to A
+    if (isB .and. compB%comp_rank == 0) then
+      call MPI_Win_lock(MPI_LOCK_SHARED, 0, 0, B2A_win, ierr)
+      call MPI_Put(compB%export_data, n, MPI_DOUBLE_PRECISION, &
+                   0, 0_MPI_ADDRESS_KIND, n, MPI_DOUBLE_PRECISION, B2A_win, ierr)
+      call MPI_Win_unlock(0, B2A_win, ierr)
+    end if
+
+    call MPI_Barrier(MPI_COMM_WORLD, ierr)
+
+    ! A pulls from B
+    if (isA .and. compA%comp_rank == 0) then
+      call MPI_Win_lock(MPI_LOCK_SHARED, 1, 0, B2A_win, ierr)
+      call MPI_Get(compA%import_data, n, MPI_DOUBLE_PRECISION, &
+                   1, 0_MPI_ADDRESS_KIND, n, MPI_DOUBLE_PRECISION, B2A_win, ierr)
+      call MPI_Win_unlock(1, B2A_win, ierr)
+    end if
+
+    if (isA) then
+      call MPI_Bcast(compA%import_data, n, MPI_DOUBLE_PRECISION, 0, comp_comm, ierr)
+    end if
+
+  end do
+
+  !----------------------------------------
+  ! Finalize
+  !----------------------------------------
+  if (coupler_comm /= MPI_COMM_NULL) then
+    call MPI_Win_free(A2B_win, ierr)
+    call MPI_Win_free(B2A_win, ierr)
+    deallocate(A2B_buf, B2A_buf)
+    call MPI_Comm_free(coupler_comm, ierr)
+  end if
+
+  if (isA) call comp_finalize(compA, "Component A")
+  if (isB) call comp_finalize(compB, "Component B")
 
   call MPI_Comm_free(comp_comm, ierr)
   call MPI_Finalize(ierr)
