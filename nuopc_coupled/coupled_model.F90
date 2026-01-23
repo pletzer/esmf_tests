1b6b634691e1c783252bf0fd12447ffe4eb5dc86
Alexander Pletzer <alexander@gokliya.net>
2026-01-23 13:20:57 +1300

    work in progress

--- nuopc_coupled/coupled_model.F90
+++ nuopc_coupled/coupled_model.F90
@@ -1,6 +1,3 @@
-! type make make coupled_model.x to compile
-! and rm PET*; mpiexec -n 5 coupled_model.x; cat PET0.ESMF_LogFile
-
 !==============================================================================
 ! ATM Component
 !==============================================================================
@@ -10,11 +7,12 @@ module ATM
   use NUOPC_Model, modelSS => SetServices
   implicit none
 
-  contains
+contains
 
   subroutine SetServices(gcomp, rc)
-    type(ESMF_GridComp)  :: gcomp
+    type(ESMF_GridComp) :: gcomp
     integer, intent(out) :: rc
+
     call NUOPC_CompDerive(gcomp, modelSS, rc=rc)
     call NUOPC_CompSpecialize(gcomp, specLabel=label_Advertise, specRoutine=Advertise, rc=rc)
     call NUOPC_CompSpecialize(gcomp, specLabel=label_RealizeProvided, specRoutine=RealizeGrid, rc=rc)
@@ -22,58 +20,51 @@ module ATM
   end subroutine
 
   subroutine Advertise(gcomp, rc)
-    type(ESMF_GridComp)  :: gcomp
+    type(ESMF_GridComp) :: gcomp
     integer, intent(out) :: rc
     type(ESMF_State) :: importState, exportState
-    ! query for importState and exportState
+
     call NUOPC_ModelGet(gcomp, importState=importState, exportState=exportState, rc=rc)
-    call NUOPC_Advertise(importState, StandardName="sea_surface_temperature", rc=rc)
-    call NUOPC_Advertise(exportState, StandardName="air_pressure_at_sea_level", rc=rc)
+    call NUOPC_Advertise(importState, standardName="sea_surface_temperature", rc=rc)
+    call NUOPC_Advertise(exportState, standardName="air_pressure_at_sea_level", rc=rc)
   end subroutine
 
   subroutine RealizeGrid(gcomp, rc)
-    type(ESMF_GridComp)  :: gcomp
+    type(ESMF_GridComp) :: gcomp
     integer, intent(out) :: rc
-    type(ESMF_Grid)      :: grid
+    type(ESMF_Grid) :: grid
     real(ESMF_KIND_R8), pointer :: coordX(:), coordY(:)
     integer :: i, lbnd(2), ubnd(2)
-    type(ESMF_State)     :: importState, exportState
-    type(ESMF_Field)     :: field_sst, field_pmsl
+    type(ESMF_State) :: importState, exportState
+    type(ESMF_Field) :: field_sst, field_pmsl
 
-    ! 1. Create the Grid and Coordinates
-    grid = ESMF_GridCreate(name="atm_grid", maxIndex=(/20,30/), &
-        coordSys=ESMF_COORDSYS_CART, regDecomp=(/2,1/), rc=rc)
+    ! Create simple 2D grid
+    grid = ESMF_GridCreate(name="atm_grid", maxIndex=(/20,30/), coordSys=ESMF_COORDSYS_CART, rc=rc)
     call ESMF_GridAddCoord(grid, staggerLoc=ESMF_STAGGERLOC_CENTER, rc=rc)
-    call ESMF_GridGetCoord(grid, coordDim=1, staggerLoc=ESMF_STAGGERLOC_CENTER, &
-                           localDe=0, farrayPtr=coordX, exclusiveLBound=lbnd, &
-                           exclusiveUBound=ubnd, rc=rc)
-    do i=lbnd(1), ubnd(1)
-        coordX(i) = (real(i, 8)-0.5_8)*(100.0_8/20.0_8)
-    end do
-    call ESMF_GridGetCoord(grid, coordDim=2, staggerLoc=ESMF_STAGGERLOC_CENTER, &
-                           localDe=0, farrayPtr=coordY, exclusiveLBound=lbnd, &
-                           exclusiveUBound=ubnd, rc=rc)
-    do i=lbnd(2), ubnd(2); coordY(i) = (real(i, 8)-0.5_8)*(100.0_8/30.0_8); end do
-    
-    ! 2. Get the States from the Component
-    call NUOPC_ModelGet(gcomp, importState=importState, exportState=exportState, rc=rc)
 
-    ! 3. Create the Fields on the Grid
-    ! Use the same StandardNames used in Advertise
-    field_sst = ESMF_FieldCreate(name="sea_surface_temperature", grid=grid, typekind=ESMF_TYPEKIND_R8, rc=rc)
+    call ESMF_GridGetCoord(grid, coordDim=1, staggerLoc=ESMF_STAGGERLOC_CENTER, farrayPtr=coordX, exclusiveLBound=lbnd, exclusiveUBound=ubnd, rc=rc)
+    do i=lbnd(1), ubnd(1)
+      coordX(i) = (real(i,8)-0.5_8)*(100.0_8/20.0_8)
+    end do
+    call ESMF_GridGetCoord(grid, coordDim=2, staggerLoc=ESMF_STAGGERLOC_CENTER, farrayPtr=coordY, exclusiveLBound=lbnd, exclusiveUBound=ubnd, rc=rc)
+    do i=lbnd(2), ubnd(2)
+      coordY(i) = (real(i,8)-0.5_8)*(100.0_8/30.0_8)
+    end do
+
+    call NUOPC_ModelGet(gcomp, importState=importState, exportState=exportState, rc=rc)
+    field_sst  = ESMF_FieldCreate(name="sea_surface_temperature", grid=grid, typekind=ESMF_TYPEKIND_R8, rc=rc)
     field_pmsl = ESMF_FieldCreate(name="air_pressure_at_sea_level", grid=grid, typekind=ESMF_TYPEKIND_R8, rc=rc)
 
-    ! 4. Realize the Fields into the NUOPC system
-    ! This attaches the created field to the NUOPC state
     call NUOPC_Realize(importState, field=field_sst, rc=rc)
     call NUOPC_Realize(exportState, field=field_pmsl, rc=rc)
   end subroutine
 
   subroutine Advance(gcomp, rc)
-    type(ESMF_GridComp)  :: gcomp
+    type(ESMF_GridComp) :: gcomp
     integer, intent(out) :: rc
     rc = ESMF_SUCCESS
   end subroutine
+
 end module ATM
 
 !==============================================================================
@@ -85,11 +76,12 @@ module OCN
   use NUOPC_Model, modelSS => SetServices
   implicit none
 
-  contains
+contains
 
   subroutine SetServices(gcomp, rc)
-    type(ESMF_GridComp)  :: gcomp
+    type(ESMF_GridComp) :: gcomp
     integer, intent(out) :: rc
+
     call NUOPC_CompDerive(gcomp, modelSS, rc=rc)
     call NUOPC_CompSpecialize(gcomp, specLabel=label_Advertise, specRoutine=Advertise, rc=rc)
     call NUOPC_CompSpecialize(gcomp, specLabel=label_RealizeProvided, specRoutine=RealizeGrid, rc=rc)
@@ -97,62 +89,50 @@ module OCN
   end subroutine
 
   subroutine Advertise(gcomp, rc)
-    type(ESMF_GridComp)  :: gcomp
+    type(ESMF_GridComp) :: gcomp
     integer, intent(out) :: rc
     type(ESMF_State) :: importState, exportState
-    ! query for importState and exportState
+
     call NUOPC_ModelGet(gcomp, importState=importState, exportState=exportState, rc=rc)
     call NUOPC_Advertise(importState, standardName="air_pressure_at_sea_level", rc=rc)
     call NUOPC_Advertise(exportState, standardName="sea_surface_temperature", rc=rc)
   end subroutine
 
   subroutine RealizeGrid(gcomp, rc)
-    type(ESMF_GridComp)  :: gcomp
+    type(ESMF_GridComp) :: gcomp
     integer, intent(out) :: rc
-    type(ESMF_Grid)      :: grid
+    type(ESMF_Grid) :: grid
     real(ESMF_KIND_R8), pointer :: coordX(:), coordY(:)
     integer :: i, lbnd(2), ubnd(2)
-    type(ESMF_State)     :: importState, exportState
-    type(ESMF_Field)     :: field_sst, field_pmsl
+    type(ESMF_State) :: importState, exportState
+    type(ESMF_Field) :: field_sst, field_pmsl
 
-    ! 1. Create the Grid and Coordinates
-    grid = ESMF_GridCreate(name="ocn_grid", maxIndex=(/30,20/), &
-        coordSys=ESMF_COORDSYS_CART, regDecomp=(/3,1/), rc=rc)
-
+    grid = ESMF_GridCreate(name="ocn_grid", maxIndex=(/30,20/), coordSys=ESMF_COORDSYS_CART, rc=rc)
     call ESMF_GridAddCoord(grid, staggerLoc=ESMF_STAGGERLOC_CENTER, rc=rc)
-    call ESMF_GridGetCoord(grid, coordDim=1, staggerLoc=ESMF_STAGGERLOC_CENTER, &
-                           localDe=0, farrayPtr=coordX, exclusiveLBound=lbnd, &
-                           exclusiveUBound=ubnd, rc=rc)
+
+    call ESMF_GridGetCoord(grid, coordDim=1, staggerLoc=ESMF_STAGGERLOC_CENTER, farrayPtr=coordX, exclusiveLBound=lbnd, exclusiveUBound=ubnd, rc=rc)
     do i=lbnd(1), ubnd(1)
-        coordX(i) = (real(i, 8)-0.5_8)*(100.0_8/30.0_8)
+      coordX(i) = (real(i,8)-0.5_8)*(100.0_8/30.0_8)
     end do
-    call ESMF_GridGetCoord(grid, coordDim=2, staggerLoc=ESMF_STAGGERLOC_CENTER, &
-                           localDe=0, farrayPtr=coordY, exclusiveLBound=lbnd, &
-                           exclusiveUBound=ubnd, rc=rc)
+    call ESMF_GridGetCoord(grid, coordDim=2, staggerLoc=ESMF_STAGGERLOC_CENTER, farrayPtr=coordY, exclusiveLBound=lbnd, exclusiveUBound=ubnd, rc=rc)
     do i=lbnd(2), ubnd(2)
-        coordY(i) = (real(i, 8)-0.5_8)*(100.0_8/20.0_8)
+      coordY(i) = (real(i,8)-0.5_8)*(100.0_8/20.0_8)
     end do
 
-    ! 2. Get the States from the Component
     call NUOPC_ModelGet(gcomp, importState=importState, exportState=exportState, rc=rc)
-
-    ! 3. Create the Fields on the Grid
-    ! Use the same StandardNames used in Advertise
-    field_sst = ESMF_FieldCreate(name="sea_surface_temperature", grid=grid, typekind=ESMF_TYPEKIND_R8, rc=rc)
+    field_sst  = ESMF_FieldCreate(name="sea_surface_temperature", grid=grid, typekind=ESMF_TYPEKIND_R8, rc=rc)
     field_pmsl = ESMF_FieldCreate(name="air_pressure_at_sea_level", grid=grid, typekind=ESMF_TYPEKIND_R8, rc=rc)
 
-    ! 4. Realize the Fields into the NUOPC system
-    ! This attaches the created field to the NUOPC state
     call NUOPC_Realize(importState, field=field_pmsl, rc=rc)
     call NUOPC_Realize(exportState, field=field_sst, rc=rc)
-
   end subroutine
 
   subroutine Advance(gcomp, rc)
-    type(ESMF_GridComp)  :: gcomp
+    type(ESMF_GridComp) :: gcomp
     integer, intent(out) :: rc
     rc = ESMF_SUCCESS
   end subroutine
+
 end module OCN
 
 !==============================================================================
@@ -162,32 +142,34 @@ module MyDriver
   use ESMF
   use NUOPC
   use NUOPC_Driver, driverSS => SetServices
-  use NUOPC_Base
   use ATM, only: atmSS => SetServices
   use OCN, only: ocnSS => SetServices
   implicit none
 
-  ! Make calendar global so it survives routine scope
   type(ESMF_Calendar) :: calendar
 
 contains
 
   subroutine SetServices(gcomp, rc)
-    type(ESMF_GridComp)  :: gcomp
+    type(ESMF_GridComp) :: gcomp
     integer, intent(out) :: rc
 
-    ! 1. Derive from base driver
     call NUOPC_CompDerive(gcomp, driverSS, rc=rc)
-
-    ! 2. Add driver specialization to set up components
-    !    Use reserved label for driver component setup
     call NUOPC_CompSpecialize(gcomp, specRoutine=SetModelComponents, rc=rc)
-
-    ! 3. Create the driver clock
     call CreateDriverClock(gcomp, rc)
   end subroutine
 
-  !------------------------------------------------------
+  subroutine SetModelComponents(gcomp, rc)
+    type(ESMF_GridComp) :: gcomp
+    integer, intent(out) :: rc
+
+    call NUOPC_DriverAddComp(gcomp, "ATM", atmSS, petList=(/0,1/), rc=rc)
+    call NUOPC_DriverAddComp(gcomp, "OCN", ocnSS, petList=(/2,3,4/), rc=rc)
+
+    ! Minimal run sequence
+    call NUOPC_DriverSetRunSequence(gcomp, slot=1, rc=rc)
+  end subroutine
+
   subroutine CreateDriverClock(gcomp, rc)
     type(ESMF_GridComp) :: gcomp
     integer, intent(out) :: rc
@@ -195,43 +177,24 @@ contains
     type(ESMF_Time) :: startTime, stopTime
     type(ESMF_TimeInterval) :: timeStep
 
-    ! Create the calendar (global)
     calendar = ESMF_CalendarCreate(rc=rc)
     call ESMF_CalendarSet(calendar, ESMF_CALKIND_GREGORIAN, rc=rc)
 
-    ! Create start/stop times
     call ESMF_TimeSet(startTime, yy=2026, mm=1, dd=22, calendar=calendar, rc=rc)
     call ESMF_TimeSet(stopTime,  yy=2026, mm=1, dd=22, calendar=calendar, h=1, rc=rc)
-
-    ! Set the timestep
     call ESMF_TimeIntervalSet(timeStep, m=15, calendar=calendar, rc=rc)
 
-    ! Create the clock and attach to the driver
     clock = ESMF_ClockCreate(timeStep, startTime, stopTime=stopTime, rc=rc)
     call ESMF_GridCompSet(gcomp, clock=clock, rc=rc)
   end subroutine
 
-  !------------------------------------------------------
-  subroutine SetModelComponents(gcomp, rc)
-    type(ESMF_GridComp)  :: gcomp
-    integer, intent(out) :: rc
-
-    ! Add components
-    call NUOPC_DriverAddComp(gcomp, "ATM", atmSS, petList=(/0,1/), rc=rc)
-    call NUOPC_DriverAddComp(gcomp, "OCN", ocnSS, petList=(/2,3,4/), rc=rc)
-
-    ! Provide the run sequence using NUOPC API
-    !call NUOPC_DriverSetRunSequence(gcomp, "runSeq: ATM -> OCN :runSeq", rc=rc)
-    call NUOPC_DriverSetRunSequence(gcomp, slot=1, rc=rc)
-
-  end subroutine
-
 end module MyDriver
 
+!==============================================================================
+! Coupled Model: ATM + OCN
+! Minimal working NUOPC example for ESMF 8.6
+!==============================================================================
 
-!==============================================================================
-! Program
-!==============================================================================
 program coupled_system
   use ESMF
   use MyDriver, only: driverSS => SetServices
@@ -239,11 +202,21 @@ program coupled_system
   integer :: rc
   type(ESMF_GridComp) :: driver
 
+  ! Initialize ESMF
   call ESMF_Initialize(rc=rc)
+
+  ! Create driver grid component
   driver = ESMF_GridCompCreate(name="Driver", rc=rc)
+
+  ! Set driver services
   call ESMF_GridCompSetServices(driver, driverSS, rc=rc)
+
+  ! Initialize, run, finalize
   call ESMF_GridCompInitialize(driver, rc=rc)
   call ESMF_GridCompRun(driver, rc=rc)
   call ESMF_GridCompFinalize(driver, rc=rc)
+
+  ! Finalize ESMF
   call ESMF_Finalize(rc=rc)
-end program
+end program coupled_system
+
