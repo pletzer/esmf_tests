95f34d3514cbcaffd6d3865309946e289f02fab9
Alexander Pletzer <alexander@gokliya.net>
2026-01-26 21:08:14 +1300

    simplifying

--- AtmOcnConProto/conn.F90
+++ AtmOcnConProto/conn.F90
@@ -46,31 +46,15 @@ module CON
 
     ! derive from NUOPC_Connector
     call NUOPC_CompDerive(connector, conSS, rc=rc)
-    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-      line=__LINE__, &
-      file=__FILE__)) &
-      return  ! bail out
 
     ! specialize connector
     call NUOPC_CompSpecialize(connector, specLabel=label_ComputeRouteHandle, &
       specRoutine=ComputeRH, rc=rc)
-    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-      line=__LINE__, &
-      file=__FILE__)) &
-      return  ! bail out
 #ifdef WITHSTATEUSE_on
     call NUOPC_CompSpecialize(connector, specLabel=label_ExecuteRouteHandle, &
       specRoutine=ExecuteRH, rc=rc)
-    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-      line=__LINE__, &
-      file=__FILE__)) &
-      return  ! bail out
     call NUOPC_CompSpecialize(connector, specLabel=label_ReleaseRouteHandle, &
       specRoutine=ReleaseRH, rc=rc)
-    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-      line=__LINE__, &
-      file=__FILE__)) &
-      return  ! bail out
 #endif
 
   end subroutine
@@ -100,19 +84,12 @@ module CON
 
     call NUOPC_ConnectorGet(connector, srcFields=srcFields, &
       dstFields=dstFields, state=state, rc=rc)
-    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-      line=__LINE__, &
-      file=__FILE__)) &
-      return  ! bail out
 
 #ifdef WITHSTATEUSE_on
     ! replicate dstFields FieldBundle in order to provide intermediate Fields
     ! - query number of fields in the FieldBundle
     call ESMF_FieldBundleGet(dstFields, fieldCount=fieldCount, rc=rc)
-    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-      line=__LINE__, &
-      file=__FILE__)) &
-      return  ! bail out
+
     ! - pull out fields in list form
     ! - !!!! MUST specify itemorderflag=ESMF_ITEMORDER_ADDORDER in order to
     !   !!!! preserve the same order as the original FieldBundle, or else the
@@ -121,84 +98,32 @@ module CON
     allocate(fields(fieldCount))
     call ESMF_FieldBundleGet(dstFields, fieldList=fields, &
       itemorderflag=ESMF_ITEMORDER_ADDORDER, rc=rc)
-    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-      line=__LINE__, &
-      file=__FILE__)) &
-      return  ! bail out
     ! - create the intermediate FieldBundle
     interDstFields = ESMF_FieldBundleCreate(name="interDstFields", rc=rc)
-    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-      line=__LINE__, &
-      file=__FILE__)) &
-      return  ! bail out
     ! - access fields one-by-one, create intermediaries, and add to new bundle
     do i=1, fieldCount
       call ESMF_FieldGet(fields(i), grid=grid, typekind=typekind, rc=rc)
-      if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-        line=__LINE__, &
-        file=__FILE__)) &
-        return  ! bail out
       field = ESMF_FieldCreate(grid, typekind, rc=rc)
-      if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-        line=__LINE__, &
-        file=__FILE__)) &
-        return  ! bail out
       call ESMF_FieldBundleAdd(interDstFields, (/field/), rc=rc)
-      if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-        line=__LINE__, &
-        file=__FILE__)) &
-        return  ! bail out
-    enddo
+   enddo
     deallocate(fields)
     ! add interDstFields to the state member
     call ESMF_StateAdd(state, (/interDstFields/), rc=rc)
-    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-      line=__LINE__, &
-      file=__FILE__)) &
-      return  ! bail out
     ! compute the first RouteHandle for srcFields->interDstFields (Regrid)
     call ESMF_FieldBundleRegridStore(srcFields, interDstFields, &
       unmappedaction=ESMF_UNMAPPEDACTION_IGNORE, routehandle=rh1, rc=rc)
-    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-      line=__LINE__, &
-      file=__FILE__)) &
-      return  ! bail out
     call ESMF_RouteHandleSet(rh1, name="src2interDstRH", rc=rc)
-    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-      line=__LINE__, &
-      file=__FILE__)) &
-      return  ! bail out
-    ! compute the second RouteHandle for interDstFields->dstFields (Redist)
+   ! compute the second RouteHandle for interDstFields->dstFields (Redist)
     call ESMF_FieldBundleRedistStore(interDstFields, dstFields, &
       routehandle=rh2, rc=rc)
-    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-      line=__LINE__, &
-      file=__FILE__)) &
-      return  ! bail out
     call ESMF_RouteHandleSet(rh2, name="interDst2dstRH", rc=rc)
-    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-      line=__LINE__, &
-      file=__FILE__)) &
-      return  ! bail out
     ! add rh1, rh2 to the state member
     call ESMF_StateAdd(state, (/rh1, rh2/), rc=rc)
-    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-      line=__LINE__, &
-      file=__FILE__)) &
-      return  ! bail out
 #else
     ! specialize with Redist, instead of the default Regrid
     call ESMF_FieldBundleRedistStore(srcFields, dstFields, &
       routehandle=rh, rc=rc)
-    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-      line=__LINE__, &
-      file=__FILE__)) &
-      return  ! bail out
     call NUOPC_ConnectorSet(connector, rh=rh, rc=rc)
-    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-      line=__LINE__, &
-      file=__FILE__)) &
-      return  ! bail out
 #endif
 
   end subroutine
@@ -223,57 +148,25 @@ module CON
 
     call NUOPC_ConnectorGet(connector, srcFields=srcFields, &
       dstFields=dstFields, state=state, driverClock=clock, rc=rc)
-    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-      line=__LINE__, &
-      file=__FILE__)) &
-      return  ! bail out
 
     ! test the parent clock
     call ESMF_ClockPrint(clock, options="currTime", &
       preString="Testing parentClock from conn.F90 ExecuteRH(): ", &
       unit=msgString, rc=rc)
-    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-      line=__LINE__, &
-      file=__FILE__)) &
-      return  ! bail out
     call ESMF_LogWrite(msgString, ESMF_LOGMSG_INFO, rc=rc)
-    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-      line=__LINE__, &
-      file=__FILE__)) &
-      return  ! bail out
 
     ! retrieve interDstFields FieldBundle from state member
     call ESMF_StateGet(state, "interDstFields", interDstFields, rc=rc)
-    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-      line=__LINE__, &
-      file=__FILE__)) &
-      return  ! bail out
     ! retrieve rh1 from state member
     call ESMF_StateGet(state, "src2interDstRH", rh1, rc=rc)
-    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-      line=__LINE__, &
-      file=__FILE__)) &
-      return  ! bail out
     ! retrieve rh2 from state member
     call ESMF_StateGet(state, "interDst2dstRH", rh2, rc=rc)
-    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-      line=__LINE__, &
-      file=__FILE__)) &
-      return  ! bail out
     ! apply rh1
     call ESMF_FieldBundleRegrid(srcFields, interDstFields, &
       routehandle=rh1, rc=rc)
-    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-      line=__LINE__, &
-      file=__FILE__)) &
-      return  ! bail out
     ! apply rh2
     call ESMF_FieldBundleRedist(interDstFields, dstFields, &
       routehandle=rh2, rc=rc)
-    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-      line=__LINE__, &
-      file=__FILE__)) &
-      return  ! bail out
 
   end subroutine
 
@@ -291,41 +184,17 @@ module CON
     rc = ESMF_SUCCESS
 
     call NUOPC_ConnectorGet(connector, state=state, rc=rc)
-    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-      line=__LINE__, &
-      file=__FILE__)) &
-      return  ! bail out
 
     ! retrieve interDstFields FieldBundle from state member
     call ESMF_StateGet(state, "interDstFields", interDstFields, rc=rc)
-    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-      line=__LINE__, &
-      file=__FILE__)) &
-      return  ! bail out
-    ! retrieve rh1 from state member
+   ! retrieve rh1 from state member
     call ESMF_StateGet(state, "src2interDstRH", rh1, rc=rc)
-    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-      line=__LINE__, &
-      file=__FILE__)) &
-      return  ! bail out
     ! retrieve rh2 from state member
     call ESMF_StateGet(state, "interDst2dstRH", rh2, rc=rc)
-    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-      line=__LINE__, &
-      file=__FILE__)) &
-      return  ! bail out
     ! release rh1
     call ESMF_FieldBundleRegridRelease(rh1, rc=rc)
-    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-      line=__LINE__, &
-      file=__FILE__)) &
-      return  ! bail out
     ! release rh2
     call ESMF_FieldBundleRegridRelease(rh2, rc=rc)
-    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-      line=__LINE__, &
-      file=__FILE__)) &
-      return  ! bail out
 
     ! Could destroy intermediate Fields and interDstFields FieldBundle here,
     ! but it is more convenient to let ESMF automatic garbage collection take
